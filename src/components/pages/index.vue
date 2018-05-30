10cbc31646c886509e2c7416b163ad1677c49d97
Albert-Lin <familywithloveg@gmail.com>
2018-05-31 07:05:20 +0800

    01. index.page init

---
+++ src/components/pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="index">
+        <Row align="middle">
+            <i-col id="i_col" :lg="24" :md="24" :sm="24" :xs="24">
+                <Row id="btnList">
+                    <Button class="orangeBtn" type="ghost" @click="showMessage('orange btn');"> BUTTON </Button>
+                </Row>
+            </i-col>
+        </Row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "index",
+    data () {
+        return {};
+    },
+    methods: {
+        showMessage (message) {
+            alert(message);
+        }
+    },
+    mounted () {
+        // ignore
+    }
+}
+</script>
+
+<style lang="scss">
+    #index {
+        #i_col {
+            height: calc(100vh - 64px);
+            background-color: #eee;
+            #btnList {
+                margin: 60vh 0 0 10vw;
+            }
+        }
+    }
+
+    .orangeBtn:hover {
+        color: #ff6600;
+        border-color: #ff6600;
+    } 
+</style>
+
+
---
+++ src/components/pages/landingPage.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="landingPage">
+        <Layout>
+            <Header id="header">HEADER</Header>
+            <Content>
+                <indexPage></indexPage>
+            </Content>
+        </Layout>
+    </div>
+</template>
+
+<script>
+import index from "./index"
+export default {
+    name: "landingPage",
+    components: {
+        indexPage: index,
+    },
+    data () {
+        return {};
+    }
+}
+</script>
+
+<style lang="scss">
+    #landingPage {
+        width: 100%;
+
+        #header {
+            color: #fff;
+        }
+    }
+</style>
+
+
